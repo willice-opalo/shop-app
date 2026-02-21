31a5642174f9cb82e63eaf3f8ce6896a5089d3e3
Willice Opalo <williceopalo11@gmail.com>
2026-02-21 11:02:34 +0300

    app config

---
+++ shop-ui/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-green-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
